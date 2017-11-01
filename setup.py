4ad32ab1d248da0618939779b570a02cb62460e7
Peter Wen <peter@whitehalmos.org>
2017-11-01 08:37:41 -0400

    First attempt at packaging

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from os import path
+
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.md')) as f:
+  long_description = f.read()
+
+setup(
+  name='emailer',
+  version='0.1.0',
+  description='Send scheduled emails',
+  long_description=long_description,
+  url='https://github.com/WhiteHalmos/emailer',
+  author='Peter Wen',
+  author_email='peter@whitehalmos.org',
+  license='MIT',
+  packages=['emailer'],
+  python_requires='>=3',
+  install_requires=[
+    'gspread',
+    'oauth2client',
+    'premailer',
+    'PyYAML',
+    'python-dateutil',
+    'requests',
+  ],
+  entry_points={
+    'console_scripts': [
+      'email = emailer.main:main',
+    ]
+  },
+)
