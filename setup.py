35f898b7f9ca02cdd82d0eb2cfb19c5ebf05853c
Peter Wen <peter@whitehalmos.org>
2018-01-07 12:44:09 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(HERE, 'README.md')) as f:
 
 setup(
   name='emailer',
-  version='0.2.4',
+  version='0.3.0',
   description='Send scheduled emails',
   long_description=long_description,
   url='https://github.com/WhiteHalmos/emailer',
