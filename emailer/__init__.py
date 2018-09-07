b7ae531673c2e836d9ec5c4724f4bce8acb6dbbd
Peter Wen <wnwen@google.com>
2018-09-07 15:15:14 -0400

    Increment dev version

--- emailer/__init__.py
+++ emailer/__init__.py
@@ -1,7 +1,7 @@
 __title__ = 'emailer'
 __description__ = 'Send scheduled emails'
 __url__ = 'https://github.com/WhiteHalmos/emailer'
-__version__ = '1.0.0'
+__version__ = '1.0.1-dev'
 __author__ = 'Peter Wen'
 __author_email__ = 'peter@whitehalmos.org'
 __license__ = 'MIT'
